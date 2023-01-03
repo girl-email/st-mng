c6c1edce4d662c5ec1da6895f644c875b5152b2f
马文卿 <529837505@qq.com>
2023-01-03 16:59:59 +0800

    chore: commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,5 +2,5 @@ import http from './http';
 import * as h from '@/types/api';
 
 export const USER_LOGIN = async (params: null) => {
-    return await http('/api/user/login', 'post', params);
+    return await http('/api/user/login', 'get', params);
 };
