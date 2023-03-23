43f715d3f988c9014b6973693944b3204247c5a7
马文卿 <529837505@qq.com>
2023-03-23 15:27:19 +0800

    feat: left menu

---
+++ src/components/layouts/components/LayoutHeader/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import { Layout } from 'antd';
+
+const LayoutHeader: FC = () => {
+    const { Header } = Layout;
+
+    return (
+        <Header>
+            
+        </Header>
+    );
+};
+
+export default LayoutHeader;
--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Layout } from 'antd';
 import LeftMenu from './components/LeftMenu';
-
+import LayoutHeader from './components/LayoutHeader';
 import './index.less';
 
 const LayoutIndex = () => {
@@ -24,12 +24,12 @@ const LayoutIndex = () => {
 	}, []);
 
 	return (
-		// 这里不用 Layout 组件原因是切换页面时样式会先错乱然后在正常显示，造成页面闪屏效果
 		<section className="container">
 			<Sider trigger={null} width={220} theme="dark">
 				<LeftMenu />
 			</Sider>
 			<Layout>
+				<LayoutHeader />
 				<Content>
 					<Outlet></Outlet>
 				</Content>
