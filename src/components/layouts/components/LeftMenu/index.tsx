c6f1f5b527e856d68c4328f48f6b74722e285402
马文卿 <529837505@qq.com>
2023-03-23 17:58:42 +0800

    fix: get user info

--- src/components/layouts/components/LeftMenu/index.tsx
+++ src/components/layouts/components/LeftMenu/index.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
+import { GET_USER_INFO } from '@/api/api';
 import './index.less';
 
 type MenuItem = Required<MenuProps>['items'][number];
@@ -23,35 +24,46 @@ function getItem(
 
 const items: MenuProps['items'] = [
   { type: 'divider' },
-    getItem('实时监控', 'sub1', null),
-    getItem('数据大屏', 'sub2', null),
-    getItem('埋点数据', 'sub3', null),
-    getItem('线上调试', 'grp', null)
-  ];
+  getItem('实时监控', 'sub1', null),
+  getItem('数据大屏', 'sub2', null),
+  getItem('埋点数据', 'sub3', null),
+  getItem('线上调试', 'grp', null)
+];
 
 const LeftMenu: FC = () => {
 
-    const onSelect: MenuProps['onSelect'] = (e) => {
-        console.log('onSelect ', e);
-      };
+  const getUserInfo = async () => {
+    const { code, data } = await GET_USER_INFO();
+    if (code === 1) {
+      localStorage.setItem('ST_USER_DATA', JSON.stringify(data));
+    }
+  };
 
-    return (
-        <nav className='st_left_nav'>
-            <div className='nav_logo'>
-                <img src="/public/icon.svg" alt="" />
-                <span>闪调</span>
-            </div>
-            <Menu
-                className='st_left_menu'
-                onSelect={onSelect}
-                style={{ width: 220 }}
-                defaultSelectedKeys={['sub1']}
-                defaultOpenKeys={['sub1']}
-                mode="inline"
-                items={items}
-            />
-        </nav>
-    );
+  const onSelect: MenuProps['onSelect'] = (e) => {
+    console.log('onSelect ', e);
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return (
+    <nav className='st_left_nav'>
+      <div className='nav_logo'>
+        <img src="/public/icon.svg" alt="" />
+        <span>闪调</span>
+      </div>
+      <Menu
+        className='st_left_menu'
+        onSelect={onSelect}
+        style={{ width: 220 }}
+        defaultSelectedKeys={['sub1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        items={items}
+      />
+    </nav>
+  );
 };
 
 export default LeftMenu;
