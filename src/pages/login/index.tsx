06073853de7a56336b14fe652f212086613b3635
马文卿 <529837505@qq.com>
2023-03-23 18:01:11 +0800

    fix: login page logo

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import LoginForm from './components/LoginForm';
 import SwitchDark from '@/components/SwitchDark';
 import loginLeft from '@/assets/images/login_left.png';
-import logo from '@/assets/images/logo.png';
 import './index.less';
 
 const Login = () => {
@@ -20,7 +19,7 @@ const Login = () => {
 				</div>
 				<div className='login-form'>
 					<div className='login-logo'>
-						<img className='login-icon' src={logo} alt='logo' />
+						<img className='login-icon' src="/public/icon.svg" alt='logo' />
 						<span className='logo-text'>闪调</span>
 					</div>
 					<LoginForm />
